f8008242adb2269cb9a2e5b5a81264118b32b51a
Richie Roche <richie.roche@gmail.com>
2026-06-27 16:26:08 +0100

    Renamed a variable to reflect actual content, added a NAN guard to move_centroids, delegated get_inertia_cost to vectorised WCSS resulting in a massive speed increase, removed redundant call to assign_centroids in restart_KMeans, removed implicit shape expansion for dataset_mean in bcss.

--- kmeans.py
+++ kmeans.py
@@ -13,8 +13,7 @@ def get_distance(query_data, query, p=2):
 
     # query_data - numpy array containing feature data
     # query - numpy array containing query
-    # returns numpy array containing the calculated distances from the query point to each of the individual feature
-    # instances
+    # returns numpy array containing the calculated distances from the query point to each individual feature instance
 
     # Minkowski - (sum (abs (xi - yi) ^ p)) ^ (1/p)
     # https://en.wikipedia.org/wiki/Minkowski_distance
@@ -45,33 +44,65 @@ def move_centroids(feature_data, centroid_indices, current_centroids):
     # Generate the new position of the centroids by calculating the mean of the datapoints assigned to each specific
     # centroid.
     # Return an array containing the new centroids
-    number_of_data_points = current_centroids.shape[1] # dimension of our data
+    number_of_features = current_centroids.shape[1]
     number_of_centroids = current_centroids.shape[0]
 
-    new_centroids = np.zeros((number_of_centroids, number_of_data_points))
+    new_centroids = np.zeros((number_of_centroids, number_of_features))
 
     # For each centroid, calculate the mean of all assigned points
     for i in range(number_of_centroids):
         # Find all points assigned to centroid i
         assigned_points = feature_data[centroid_indices == i]
-        new_centroids[i] = np.mean(assigned_points, axis=0)
+        # Guard - if a centroid ends up with no assigned points, np.mean of empty array returns NAN
+        if len(assigned_points) == 0:
+            new_centroids[i] = current_centroids[i] # keep centroid in place
+        else:
+            new_centroids[i] = np.mean(assigned_points, axis=0)
 
     return new_centroids
 
+
+def wcss(feature_data, centroid_indices, current_centroids):
+    # Within Cluster Sum of Squares
+    # the sum of the squared Euclidean distances between the data points and their respective centroids
+
+    # abs(x - c_i) ^ 2
+    # Get the centroid for every point based on its cluster assignment
+    assigned_centroids = current_centroids[centroid_indices]
+
+    # squared distances from every point to its assigned centroid
+    squared_distances = get_distance(feature_data, assigned_centroids) ** 2
+
+    # Sum all squared distances
+    return np.sum(squared_distances)
+
+
 def get_inertia_cost(feature_data, centroid_indices, current_centroids):
     # Basically - average distance from points to centroids
-
     # Take in as arguments the feature data, an array containing the centroid indices assigned to each feature instance,
     # and the current array of centroids.
     # Calculate and return the current inertia (distortion cost) function
-    inertia_cost = 0.0
+    # Delegate to wcss as wcss is already vectorised
+    return wcss(feature_data, centroid_indices, current_centroids)
 
-    for i in range(feature_data.shape[0]):
-        # Get the centroid this point is assigned to
-        assigned_centroid = current_centroids[centroid_indices[i]]
-        inertia_cost += get_distance(feature_data[i:i + 1], assigned_centroid)[0] ** 2
 
-    return inertia_cost
+def bcss(feature_data, centroid_indices, current_centroids):
+    # BCSS - Between-Cluster Sum of Squares - "the weighted sum of squared Euclidean distances between each cluster
+    # centroid (mean) and the overall data centroid (mean)" https://en.wikipedia.org/wiki/Calinski%E2%80%93Harabasz_index
+    # n_i number of points in a cluster
+    # c_i is the current centroid
+    # u - overall centroid / mean
+    # BCSS = Sum n_i (abs(c_i - u))^2
+
+    dataset_mean = np.mean(feature_data, axis=0).reshape(1, -1)  # explicit 2D reshape for get_distance (was implicit)
+    # How many points are assigned to each cluster
+    clusters, cluster_counts = np.unique(centroid_indices, return_counts=True)
+
+    # squared distances from each centroid to overall mean
+    distances = get_distance(current_centroids, dataset_mean) ** 2
+    # weighted sum of squared distances
+    return np.sum(cluster_counts * distances)
+
 
 def calculate_ch_cost(feature_data, centroid_indices, current_centroids):
     # centroid_indices - array matching centroid number to feature data
@@ -94,38 +125,6 @@ def calculate_ch_cost(feature_data, centroid_indices, current_centroids):
     cost = numerator/denominator
     return cost
 
-def bcss(feature_data, centroid_indices, current_centroids):
-
-    # BCSS - Between-Cluster Sum of Squares - "the weighted sum of squared Euclidean distances between each cluster
-    # centroid (mean) and the overall data centroid (mean)" https://en.wikipedia.org/wiki/Calinski%E2%80%93Harabasz_index
-    # n_i number of points in a cluster
-    # c_i is the current centroid
-    # u - overall centroid / mean
-    # BCSS = Sum n_i (abs(c_i - u))^2
-
-    dataset_mean = np.mean(feature_data, axis=0)
-    # How many points are assigned to each cluster
-    clusters, cluster_counts = np.unique(centroid_indices, return_counts=True)
-
-    # squared distances from each centroid to overall mean
-    distances = get_distance(current_centroids, dataset_mean) ** 2
-    # weighted sum of squared distances
-    return np.sum(cluster_counts * distances)
-
-
-def wcss(feature_data, centroid_indices, current_centroids):
-    # Within Cluster Sum of Squares
-    # the sum of the squared Euclidean distances between the data points and their respective centroids
-
-    # abs(x - c_i) ^ 2
-    # Get the centroid for every point based on its cluster assignment
-    assigned_centroids = current_centroids[centroid_indices]
-
-    # squared distances from every point to its assigned centroid
-    squared_distances = get_distance(feature_data, assigned_centroids) ** 2
-
-    # Sum all squared distances
-    return np.sum(squared_distances)
 
 def restart_KMeans(feature_data, number_of_centroids, iterations, restarts):
 
@@ -142,18 +141,17 @@ def restart_KMeans(feature_data, number_of_centroids, iterations, restarts):
             new_centroids = move_centroids(feature_data , centroid_indices, current_centroids)
             current_centroids = new_centroids
 
-        # Need the indexes so call assign_centroids again
-        current_iteration_indices = assign_centroids(feature_data, current_centroids)
-        current_iteration_cost = get_inertia_cost(feature_data, current_iteration_indices, current_centroids)
+        # After all inner iterations, centroid_indices holds the result of the final iteration
+        current_iteration_cost = get_inertia_cost(feature_data, centroid_indices, current_centroids)
 
         if i == 1 or current_iteration_cost < best_cost:
             best_centroids = current_centroids
-            best_indices = current_iteration_indices
+            best_indices = centroid_indices
             best_cost = current_iteration_cost
 
     ch_cost = calculate_ch_cost(feature_data, best_indices, best_centroids)
-
     return best_centroids, best_cost, ch_cost
+
 
 def main():
     best_centroids = []
